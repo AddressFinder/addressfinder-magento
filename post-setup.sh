0a172deb05bf267d6faa55acda9d3906d5027d12
Naiki <naikipl@gmail.com>
2017-05-09 13:07:11 +1200

    Update docker setup

--- post-setup.sh
+++ post-setup.sh
@@ -2,14 +2,24 @@
 
 if ! [ -d "./html" ]; then
   containerid=$(docker ps | grep addressfindermagento_app | awk '{print $1}')
-  echo 'Copying Magento files to your local machine'
+  echo 'Copying Magento files to your local machine (for reference only)'
   docker cp $containerid:/var/www/html ./
 fi
 
-if ! [ -L "./html/app/code/AddressFinder" ]; then
-  echo 'Symlinking the AddressFinder module'
-  ln -s ../../../AddressFinder/ html/app/code/AddressFinder
-fi
-
 echo 'Uncommenting app volumes in docker-compose.yml'
-sed -i '' 's/# - \.\/html\/app/- .\/html\/app/' docker-compose.yml
+sed -i '' 's/# - \.\/AddressFinder/- .\/AddressFinder/' docker-compose.yml
+docker-compose up -d app
+
+echo 'Disabling cache types for development purposes'
+docker-compose exec phpfpm ./bin/magento cache:disable
+docker-compose exec phpfpm ./bin/magento cache:clean
+docker-compose exec phpfpm ./bin/magento cache:flush
+
+echo 'Setting Magento mode to Developer'
+docker-compose exec phpfpm ./bin/magento deploy:mode:set developer
+
+if ! [ -L "./html/code/AddressFinder" ]; then
+  echo 'Installing the AddressFinder module'
+  docker-compose exec phpfpm ./bin/magento setup:upgrade
+  docker-compose exec phpfpm ./bin/magento setup:di:compile
+fi
