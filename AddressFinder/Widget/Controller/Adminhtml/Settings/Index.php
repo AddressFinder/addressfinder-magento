6daf5714230fce3b88032ec9494e9d1c88a9974b
Naiki <naikipl@gmail.com>
2017-05-24 13:03:05 +1200

    WIP hooking up the form on admin settings page

--- AddressFinder/Widget/Controller/Adminhtml/Settings/Index.php
+++ AddressFinder/Widget/Controller/Adminhtml/Settings/Index.php
@@ -15,11 +15,11 @@
      * @param \Magento\Framework\View\Result\PageFactory $resultPageFactory
      */
     public function __construct(
-        \Magento\Backend\App\Action\Context $context,
-        \Magento\Framework\View\Result\PageFactory $resultPageFactory
+      \Magento\Backend\App\Action\Context $context,
+      \Magento\Framework\View\Result\PageFactory $resultPageFactory
     ) {
-         parent::__construct($context);
-         $this->resultPageFactory = $resultPageFactory;
+      parent::__construct($context);
+      $this->resultPageFactory = $resultPageFactory;
     }
 
     /**
@@ -29,7 +29,40 @@
      */
     public function execute()
     {
-         return  $resultPage = $this->resultPageFactory->create();
+      $post = $this->getRequest()->getPost();
+      if ($post) {
+
+        // Retrieve your form data
+        $auLicenceKey = $post['au_licence_key'];
+        $nzLicenceKey = $post['nz_licence_key'];
+
+        // $email = $this->getRequest()->getParam('email_address');
+
+        $settings = $this->settingsDataFactory->create();
+
+        try {
+          $settingsData = $settings->load(1);
+          $settingsData->setAuLicenceKey($auLicenceKey);
+          $settingsData->setNzLicenceKey($nzLicenceKey);
+          $settingsData->save();
+        } catch(\Exception $ex) {
+          var_dump($ex.getMessage);
+        }
+
+        // Display the succes form validation message
+        $this->messageManager->addSuccess('Settings saved!');
+
+        // // Redirect to your form page (or anywhere you want...)
+        // $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
+        // $resultRedirect->setUrl('/compagny/module/contact');
+        //
+        // return $resultRedirect;
+      }
+      // Render the page
+      // $this->_view->loadLayout();
+      // $this->_view->renderLayout();
+
+      return  $resultPage = $this->resultPageFactory->create();
     }
   }
 ?>
