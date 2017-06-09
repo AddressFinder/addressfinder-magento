a838e11debaa17d950e6a1cadecb4c52e6d8a80b
Naiki <naikipl@gmail.com>
2017-06-09 12:53:59 +1200

    Add DebugMode config in admin

--- AddressFinder/Widget/Helper/Data.php
+++ AddressFinder/Widget/Helper/Data.php
@@ -19,9 +19,15 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper
       parent::__construct($context);
   }
 
+  public function getEncryptedConfig($field)
+  {
+    $value = $this->getConfig($field);
+    return $this->encryptor->decrypt($value);
+  }
+
   public function getConfig($field)
   {
     $value = $this->scopeConfig->getValue("AddressFinder_Widget/settings/" . $field, \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
-    return $this->encryptor->decrypt($value);
+    return $value;
   }
 }
