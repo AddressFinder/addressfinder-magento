22ce6fae2d62635ea6629bf9a8ff654c8e29b5c3
Naiki <naikipl@gmail.com>
2017-06-13 14:29:43 +1200

    Split js into separate js file. Still needs testing

--- AddressFinder/Widget/view/frontend/requirejs-config.js
+++ AddressFinder/Widget/view/frontend/requirejs-config.js
@@ -1,7 +1,8 @@
 var config = {
   paths: {
+    addressfinder_magento: 'AddressFinder_Widget/js/addressfinder_magento',
     addressfinder: 'https://api.addressfinder.io/assets/v3/core',
-    reqwest:       'https://files-abletech-nz.s3.amazonaws.com/addressfinder/reqwest',
+    reqwest: 'https://files-abletech-nz.s3.amazonaws.com/addressfinder/reqwest',
     neat_complete: 'https://files-abletech-nz.s3.amazonaws.com/addressfinder/neat-complete'
   },
 };
