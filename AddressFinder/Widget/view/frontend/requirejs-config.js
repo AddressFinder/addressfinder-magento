6aef71a0690998de9fb5f81973bd81f0d303209f
Naiki <naikipl@gmail.com>
2017-06-14 13:02:58 +1200

    Update paths to reqwest.js and neat-complete.js

--- AddressFinder/Widget/view/frontend/requirejs-config.js
+++ AddressFinder/Widget/view/frontend/requirejs-config.js
@@ -2,7 +2,7 @@ var config = {
   paths: {
     addressfinder_magento: 'AddressFinder_Widget/js/addressfinder_magento',
     addressfinder: 'https://api.addressfinder.io/assets/v3/core',
-    reqwest: 'https://files-abletech-nz.s3.amazonaws.com/addressfinder/reqwest',
-    neat_complete: 'https://files-abletech-nz.s3.amazonaws.com/addressfinder/neat-complete'
+    reqwest: 'https://api.addressfinder.io/assets/reqwest/v1/reqwest',
+    neat_complete: 'https://api.addressfinder.io/assets/neat-complete/v1/neat-complete'
   },
 };
