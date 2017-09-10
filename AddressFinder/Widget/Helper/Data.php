8c6824cdb1cfe36ebc7889e24719322dffca4847
Naiki <naikipl@gmail.com>
2017-09-11 09:54:11 +1200

    Edit php code to conform with standards

--- AddressFinder/Widget/Helper/Data.php
+++ AddressFinder/Widget/Helper/Data.php
@@ -4,30 +4,33 @@ namespace AddressFinder\Widget\Helper;
 
 class Data extends \Magento\Framework\App\Helper\AbstractHelper
 {
-  protected $storeManager;
-  protected $objectManager;
-  protected $encryptor;
+    private $storeManager;
+    private $objectManager;
+    private $encryptor;
+    private $storeScope = \Magento\Store\Model\ScopeInterface::SCOPE_STORE;
+    private $settingsNamespace = "AddressFinder_Widget/settings/";
 
-  public function __construct(\Magento\Framework\App\Helper\Context $context,
-      \Magento\Framework\ObjectManagerInterface $objectManager,
-      \Magento\Store\Model\StoreManagerInterface $storeManager,
-      \Magento\Framework\Encryption\EncryptorInterface $encryptor
-  ) {
-      $this->objectManager = $objectManager;
-      $this->storeManager  = $storeManager;
-      $this->encryptor = $encryptor;
-      parent::__construct($context);
-  }
+    public function __construct(
+        \Magento\Framework\App\Helper\Context $context,
+        \Magento\Framework\ObjectManagerInterface $objectManager,
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        \Magento\Framework\Encryption\EncryptorInterface $encryptor
+    ) {
+        $this->objectManager = $objectManager;
+        $this->storeManager  = $storeManager;
+        $this->encryptor = $encryptor;
+        parent::__construct($context);
+    }
 
-  public function getEncryptedConfig($field)
-  {
-    $value = $this->getConfig($field);
-    return $this->encryptor->decrypt($value);
-  }
+    public function getEncryptedConfig($field)
+    {
+        $value = $this->getConfig($field);
+        return $this->encryptor->decrypt($value);
+    }
 
-  public function getConfig($field)
-  {
-    $value = $this->scopeConfig->getValue("AddressFinder_Widget/settings/" . $field, \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
-    return $value;
-  }
+    public function getConfig($field)
+    {
+        $value = $this->scopeConfig->getValue($settingsNamespace . $field, $storeScope);
+        return $value;
+    }
 }
