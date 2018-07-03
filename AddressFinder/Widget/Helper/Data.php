357b50b3e077d925c2904b1428e593abc40c38cc
Naiki <naikipl@gmail.com>
2018-07-04 10:28:00 +1200

    No need to encrypt the licence key

--- AddressFinder/Widget/Helper/Data.php
+++ AddressFinder/Widget/Helper/Data.php
@@ -6,24 +6,15 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper
 {
     private $storeManager;
     private $objectManager;
-    private $encryptor;
 
     public function __construct(
         \Magento\Framework\App\Helper\Context $context,
         \Magento\Framework\ObjectManagerInterface $objectManager,
-        \Magento\Store\Model\StoreManagerInterface $storeManager,
-        \Magento\Framework\Encryption\EncryptorInterface $encryptor
+        \Magento\Store\Model\StoreManagerInterface $storeManager
     ) {
         $this->objectManager = $objectManager;
         $this->storeManager  = $storeManager;
-        $this->encryptor = $encryptor;
         parent::__construct($context);
-    }
-
-    public function getEncryptedConfig($field)
-    {
-        $value = $this->getConfig($field);
-        return $this->encryptor->decrypt($value);
     }
 
     public function getConfig($field)
