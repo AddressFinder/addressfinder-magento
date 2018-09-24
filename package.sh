1c1e524a435c860633c5e07b858a531a0ab34551
Ben Corlett <bencorlett@me.com>
2018-09-24 10:17:14 +1000

    Tidies up the packaging process to remove unneeded files.

--- package.sh
+++ package.sh
@@ -9,4 +9,4 @@ sed -i '' "s/\"version\": \".*\"/\"version\": \""$version"\"/" composer.json
 sed -i '' "s/\"version\": \".*\"/\"version\": \""$version"\"/" package.json
 sed -i '' "s/setup_version=\".*\"/setup_version=\""$version"\"/" etc/module.xml
 
-zip -r "../../addressfinder_addressfinder-"$version".zip" ./* -x '.git/*' -x 'node_modules/*'
+git archive -o "addressfinder_addressfinder-"$version".zip" HEAD
